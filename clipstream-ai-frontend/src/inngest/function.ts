6e8ac754bf9185bcf9fc6533c2124c49d0d78d22
Sarthak Harshe <harshe@usc.edu>
2025-07-06 12:29:19 -0700

    feat: add Next.js frontend with T3 stack
    - Create starter  Next.js frontend application using T3 stack

---
+++ clipstream-ai-frontend/src/inngest/function.ts
@@ -0,0 +1,22 @@
+import { inngest } from "./client";
+import { env } from "~/env";
+
+export const processVideo = inngest.createFunction(
+  { id: "process-video", concurrency: {
+    limit: 1,
+    key: "event.data.userId"
+  } },
+  { event: "process-video-events" },
+  async ({ event, step }) => {
+    await step.run("call-modal-endpoint", async () => {
+        await fetch(env.PROCESS_VIDEO_ENDPOINT, {
+            method: "POST",
+            body: JSON.stringify({s3_key: "test1/Blocks5mins.mp4"}),
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${env.PROCESS_VIDEO_ENDPOINT_AUTH}`,
+            },
+        });
+    });
+  },
+);
